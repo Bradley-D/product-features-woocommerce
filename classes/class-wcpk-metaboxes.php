9c6756bd56e0037f1c719f75e3127545d859d1b4
Bradley Davis <bd@bradley-davis.com>
2015-07-08 12:41:34 +1000

    [ ADD ] Metabox cleanup. WC short desc bug to fix

--- classes/class-wcpk-metaboxes.php
+++ classes/class-wcpk-metaboxes.php
@@ -25,8 +25,10 @@ class Wcpk_Metaboxes {
 	}
 
 	function init() {
+		// Product Key
 		add_action( 'add_meta_boxes', array( $this, 'wcpk_add_metabox' ) );
 		add_action( 'save_post', array( $this, 'wcpk_save_metabox' ) );
+		// WooCommerce Product
 		add_action( 'add_meta_boxes', array( $this, 'wcpk_wc_add_metabox' ) );
 		add_action( 'save_post', array( $this, 'wcpk_wc_save_metabox' ) );
 	}
@@ -35,19 +37,15 @@ class Wcpk_Metaboxes {
 	 * Product Key: Adds metabox container
 	 * @since 1.0
 	 */
-	function wcpk_add_metabox( $post_type ) {
-	
-	    $post_types = array( 'product-key' );
-	    if ( in_array( $post_type, $post_types ) ) :
-			add_meta_box(
-				'wcpk_metaboxes', 
-				__( 'WooCommerce Product Key', 'wcpk' ),
-				array( $this, 'wcpk_render_metabox_content' ),
-				$post_type,
-				'advanced',
-				'high'
-			);
-	    endif;
+	function wcpk_add_metabox() {
+		add_meta_box(
+			'wcpk_metaboxes', 
+			__( 'WooCommerce Product Key', 'wcpk' ),
+			array( $this, 'wcpk_render_metabox_content' ),
+			'product-key',
+			'normal',
+			'low'
+		);
 	}
 
 	/**
@@ -131,136 +129,40 @@ class Wcpk_Metaboxes {
 	 * WC Product: Adds metabox container
 	 * @since 1.0
 	 */
-	function wcpk_wc_add_metabox( $post_type ) {
-	
-	    $post_types = array( 'product' );
-	    if ( in_array( $post_type, $post_types ) ) :
-			add_meta_box(
-				'wcpk_wc_metaboxes', 
-				__( 'Select Product Key', 'wcpk' ),
-				array( $this, 'wcpk_wc_render_metabox_content' ),
-				$post_type,
-				'side',
-				'default'
-			);
-	    endif;
+	function wcpk_wc_add_metabox() {	
+		add_meta_box(
+			'wcpk_wc_metaboxes', 
+			__( 'Select Product Key', 'wcpk' ),
+			array( $this, 'wcpk_wc_render_metabox_content' ),
+			'product',
+			'side',
+			'default'
+		);
 	}
 
 	/**
 	 * WC Product: Save the meta when the post is saved.
 	 * @since 1.0
 	 */
-	public function wcpk_wc_save_metabox( $post_id ) {
-	
-		/*
-		 * We need to verify this came from the our screen and with proper authorization,
-		 * because save_post can be triggered at other times.
-		 */
-
-		// Check if our nonce is set.
-		if ( ! isset( $_POST['wcpk_inner_custom_box_nonce'] ) ) :
-			return $post_id;
-		endif;
-
-		$nonce = $_POST['wcpk_inner_custom_box_nonce'];
-
-		// Verify that the nonce is valid.
-		if ( ! wp_verify_nonce( $nonce, 'wcpk_inner_custom_box' ) ) :
-			return $post_id;
-		endif;
-
-		// If this is an autosave, our form has not been submitted, so we don't want to do anything.
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) :
-			return $post_id;
-		endif;
-
-		// Check the user's permissions.
-		if ( 'page' == $_POST['post_type'] ) :
-
-			if ( ! current_user_can( 'edit_page', $post_id ) ) :
-				return $post_id;
-			endif;
-	
-		else :
-
-			if ( ! current_user_can( 'edit_post', $post_id ) ) :
-				return $post_id;
-			endif;
-
-		endif;
-
-		/* OK, its safe for us to save the data now. */
-		if ( ! isset( $_POST['wcpk_select_field'] ) ) :
-			return;
-		endif;
-
-		// Sanitize the user input.
-		// Need to make a WP_Query to get all product key slugs
-		$wcpk_select_args = array(
-			'post_type' => 'product-key',
-		);
-		
-		$wcpk_select_query = new WP_Query( $wcpk_select_args );
-
-		$wcpk_valid_selects = array();
-
-		if ( $wcpk_select_query->have_posts() ) :
-			while ( $wcpk_select_query->have_posts() ) : $wcpk_select_query->the_post();
-
-				$wcpk_valid_slug = get_post();
-				$wcpk_valid_selects[] = $wcpk_valid_slug->post_name;
-
-			endwhile;
-		endif;
-
-		$wcpk_select_data = sanitize_text_field( $_POST['wcpk_select_field'] );
-
-		if ( in_array( $wcpk_select_data, $wcpk_valid_selects ) ) :
-
-			update_post_meta( $post->ID, 'wcpk_select_field', $wcpk_select_data );
-
-		endif;
-		// Restore the data
-		wp_reset_postdata();
-
-	}
+	public function wcpk_wc_save_metabox( $post_id ) {}
 
 	public function wcpk_wc_render_metabox_content( $post ) {
-	
-		// Add an nonce field so we can check for it later.
-		wp_nonce_field( 'wcpk_inner_custom_box', 'wcpk_inner_custom_box_nonce' );
 
-		// Use get_post_meta to retrieve an existing value from the database.
-		//$wcpk_text_field = get_post_meta( $post->ID, '_wcpk_text_field_meta_value_key', true );
+		$wcpk_product_key_args = array(
+			'post_type' => 'product-key'
+		);
 
-		// Display the select list
-		?>
-		<p>To select multiple product keys, hold down the Ctrl (Windows) or Command (Mac) button to select multiple options.</p>
-		<p>
-	        <label for="wcpk_select_label"><strong>Select Product Keys: </strong></label></p><p>
-	        <select multiple="multiple" name="wcpk_select_field[]" id="wcpk_select_field" style="width: 100%;"><?php
+		$wcpk_product_key = new WP_Query( $wcpk_product_key_args );
 
-	        $wcpk_product_keys_args = array(
-				'post_type' => 'product-key',
-			);
-		
-			$wcpk_product_keys = new WP_Query( $wcpk_product_keys_args );
+		if ( $wcpk_product_key->have_posts() ) :  ?>
+			<select name="" id=""><?php
+			while ( $wcpk_product_key->have_posts() ) : $wcpk_product_key->the_post(); ?>
+				<option value="<?php echo get_the_ID(); ?>"><?php the_title(); ?></option>;<?php
+			endwhile; ?>
+			</select><?php
+		endif;
 
-			$wcpk_select_selected = get_option( 'wcpk_select_field', array() );
-
-	        if ( $wcpk_product_keys->have_posts() ) :
-				while ( $wcpk_product_keys->have_posts() ) : $wcpk_product_keys->the_post(); 
-
-					$wcpk_select_slug = get_post(); ?>
-					<option value="<?php echo esc_attr( $wcpk_select_slug->post_name ); ?>" <?php echo selected( in_array( $wcpk_select_slug->post_name, $wcpk_select_selected ) ); ?> ><?php echo '-&nbsp;' . esc_attr( get_the_title() ); ?></option><?php
-				
-				endwhile;
-			endif; ?>
-
-
-	        </select>
-    	</p><?php
-    	// Restore the data
+		/* Restore original Post Data */
 		wp_reset_postdata();
 	}
 }
