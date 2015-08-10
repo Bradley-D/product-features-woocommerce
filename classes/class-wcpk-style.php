df39e12451f2292b091f0fa4c69d9e565ef4acfa
Bradley Davis <bd@bradley-davis.com>
2015-08-10 21:26:12 +1000

    [ ADDED ] customiser output

--- classes/class-wcpk-style.php
+++ classes/class-wcpk-style.php
@@ -27,34 +27,37 @@ class Wcpk_Style {
      * Customizer styles
      * @since 1.0
      */
-    function wcpk_customizer_css() { ?>
+    function wcpk_customizer_css() {
+        // Get WCPK Customizer settings as array
+        if ( '' != get_theme_mod( 'wcpk_customizer' ) ) :
+            $wcpk_customizer_settings = get_theme_mod( 'wcpk_customizer' );
+        endif; ?>
+
         <style type="text/css">
             /* Feature Image Options */
-            <?php if ( '' != get_theme_mod( 'wcpk_image_border' ) ) : ?>
-            <?php endif; ?>
-
-            <?php if ( '' != get_theme_mod( 'wcpk_border_color' ) ) : ?>
-            <?php endif; ?>
-
-            <?php if ( '' != get_theme_mod( 'wcpk_image_padding' ) ) : ?>
-            <?php endif; ?>
-
-            <?php if ( '' != get_theme_mod( 'wcpk_image_bg' ) ) : ?>
-            <?php endif; ?>
-
-            /* Font Awesome Options */
-            <?php if ( '' != get_theme_mod( 'wcpk_fa_size' ) ) : ?>
-            <?php endif; ?>
-
+            .wcpk_item {
+                /* Image Width */
+                max-width: <?php echo $wcpk_customizer_settings['image_width']; ?>;
+                /* Border Width */
+                border-width: <?php echo $wcpk_customizer_settings['image_border']; ?>;
+                /* Border Color */
+                border-color: <?php echo $wcpk_customizer_settings['border_color']; ?>;
+                /* Image Padding*/
+                padding: <?php echo $wcpk_customizer_settings['image_padding']; ?>;
+                /* Image Background */
+                background-color: <?php echo $wcpk_customizer_settings['image_bg']; ?>;
+                /* Font Awesome Options */ <?php 
+                if ( 'wcpk_image_font' == $wcpk_customizer_settings['key_type'] ) : ?>
+                    font-size: <?php echo $wcpk_customizer_settings['fa_size']; ?>;     
+                <?php endif; ?>
+            }
             /* Tooltip Options */
-            <?php if ( '' != get_theme_mod( 'wcpk_tooltip_font_color' ) ) : ?>
-            <?php endif; ?>
-
-            <?php if ( '' != get_theme_mod( 'wcpk_tooltip_bg' ) ) : ?>
-            <?php endif; ?>
-
-            <?php if ( '' != get_theme_mod( 'wcpk_tooltip_opacity' ) ) : ?>
-            <?php endif; ?>
+            .wcpk-tooltip:hover:before {
+                /* Font Color */
+                color: <?php echo $wcpk_customizer_settings['tooltip_font_color']; ?>;
+                /* Background Color */
+                background-color: <?php echo $wcpk_customizer_settings['tooltip_bg']?>;
+            }
         </style> <?php
     }
 } // END Wcpk_Style class
