9e9848dfc4971d99971b3c6baef2584a074e3aff
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-06-06 13:35:19 +1000

    [ UPDATE ] clean up

--- classes/class-pffwc-settings.php
+++ classes/class-pffwc-settings.php
@@ -37,46 +37,22 @@ class Pffwc_Settings {
 	 * @since 1.0
 	 */
 	public function pffwc_add_settings( $wp_customize ) {
-		// Add pffwc panel
-		$wp_customize->add_panel( 'pffwc_settings',
-			array(
-				'title'       => __( 'Product Feature Settings', 'pffwc' ),
-				'description' => __( 'Personalize your product features.', 'pffwc' ),
-				'priority'    => 1000,
-			)
-		);
-		// General options section
+		// Add section to WooCommerce Panel
 		$wp_customize->add_section( 'pffwc_options',
 			array(
-				'title'      => __( 'General Options', 'pffwc' ),
-			  	'panel'      => 'pffwc_settings',
-			  	'capability' => '',
-			  	'priority'   => 1,
-			)
-		);
-		// Feature Image Section
-		$wp_customize->add_section( 'pffwc_options_fi',
-			array(
-				'title'      => __( 'Feature Image Settings', 'pffwc' ),
-				'panel'      => 'pffwc_settings',
+				'title'      => __( 'Product Features', 'pffwc' ),
+				'panel'      => 'woocommerce',
+				'type'       => 'option',
 				'capability' => '',
-				'priority'   => 2,
+				'priority'   => 500,
 			)
 		);
-		// Tooltip Section
-		$wp_customize->add_section( 'pffwc_options_tt',
-			array(
-				'title'      => __( 'Tooltip Settings', 'pffwc' ),
-				'panel'      => 'pffwc_settings',
-				'capability' => '',
-				'priority'   => 4,
-			)
-		);
+
 		/*
 		 * General options
 		 * @since 1.0
 		 */
-		// Choose PK Location
+		// Choose Product Feature location
 		$wp_customize->add_setting( 'pffwc_render_location',
 			array(
 				'default' => 'pffwc_after_short_desc',
@@ -85,12 +61,12 @@ class Pffwc_Settings {
 		);
 		$wp_customize->add_control( 'pffwc_render_location',
 			array(
-				'label'    => __( 'Choose Display Location', 'pffwc' ),
+				'label'    => __( 'Product Feature Location', 'pffwc' ),
+				'description' => __( 'Choose the location that you would like the product feature to display.', 'pffwc'),
 				'settings' => 'pffwc_render_location',
 				'section'  => 'pffwc_options',
 				'type'     => 'select',
 				'choices'  => array(
-					'pffwc_after_gallery'      => __( 'After product gallery', 'pffwc' ),
 					'pffwc_after_heading'      => __( 'After product heading', 'pffwc' ),
 					'pffwc_after_price'        => __( 'After product price', 'pffwc' ),
 					'pffwc_after_short_desc'   => __( 'After short description', 'pffwc' ),
@@ -99,171 +75,10 @@ class Pffwc_Settings {
 				),
 			)
 		);
-
-		/*
-		 * Feature Image Options
-		 * @since 1.0
-		 */
-		// Image Width
-		$wp_customize->add_setting( 'pffwc_image_width',
-			array(
-				'default'           => '16.5%',
-				'sanitize_callback' => array( $this, 'pffwc_sanitize_image_width' ),
-			)
-		);
-		$wp_customize->add_control( 'pffwc_image_width',
-			array(
-				'label'    => __( 'Choose Image Width', 'pffwc' ),
-				'settings' => 'pffwc_image_width',
-				'section'  => 'pffwc_options_fi',
-				'type'     => 'select',
-				'choices'  => array(
-					'12.5%'  => __( '8 Per Row', 'pffwc' ), // 8
-					'16.5%'  => __( '6 Per Row', 'pffwc' ), // 6
-					'20%'    => __( '5 Per Row', 'pffwc' ), // 5
-					'25%'    => __( '4 Per Row', 'pffwc' ), // 4
-					'33.33%' => __( '3 Per Row', 'pffwc' ), // 3
-					'50%'    => __( '2 Per Row', 'pffwc' ), // 2
-				),
-			)
-		);
-		// Border Width
-		$wp_customize->add_setting( 'pffwc_image_border',
-			array(
-				'default'           => 'pffwc_image_border_zero',
-				'sanitize_callback' => array( $this, 'pffwc_sanitize_border_width' ),
-			)
-		);
-		$wp_customize->add_control( 'pffwc_image_border',
-			array(
-				'label'     => __( 'Image Border Width', 'pffwc' ),
-				'settings'  => 'pffwc_image_border',
-				'section'   => 'pffwc_options_fi',
-				'type'      => 'select',
-				'choices'   => array(
-					'pffwc_image_border_zero'   => __( '0px', 'pffwc' ),
-					'pffwc_image_border_one'    => __( '1px', 'pffwc' ),
-					'pffwc_image_border_two'    => __( '2px', 'pffwc' ),
-					'pffwc_image_border_three'  => __( '3px', 'pffwc' ),
-					'pffwc_image_border_four'   => __( '4px', 'pffwc' ),
-					'pffwc_image_border_five'   => __( '5px', 'pffwc' ),
-					'pffwc_image_border_six'    => __( '6px', 'pffwc' ),
-					'pffwc_image_border_seven'  => __( '7px', 'pffwc' ),
-					'pffwc_image_border_eight'  => __( '8px', 'pffwc' ),
-				),
-			)
-		);
-		// Border Color
-		$wp_customize->add_setting( 'pffwc_border_color',
-			array(
-				'default'           => '#ddd',
-				'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
-		$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'pffwc_border_color',
-			array(
-				'label'    => __( 'Border Color', 'pffwc' ),
-				'settings' => 'pffwc_border_color',
-				'section'  => 'pffwc_options_fi',
-			)
-		) );
-		// Image Padding
-		$wp_customize->add_setting( 'pffwc_image_padding',
-			array(
-				'default'           => 'pffwc_image_padding_zero',
-				'sanitize_callback' => array( $this, 'pffwc_sanitize_image_padding' ),
-			)
-		);
-		$wp_customize->add_control( 'pffwc_image_padding',
-			array(
-				'label'      => __( 'Image Padding', 'pffwc' ),
-				'settings'   => 'pffwc_image_padding',
-				'section'    => 'pffwc_options_fi',
-				'type'       => 'select',
-				'choices'    => array(
-					'pffwc_image_padding_zero'   => __( '0px', 'pffwc' ),
-					'pffwc_image_padding_one'    => __( '1px', 'pffwc' ),
-					'pffwc_image_padding_two'    => __( '2px', 'pffwc' ),
-					'pffwc_image_padding_three'  => __( '3px', 'pffwc' ),
-					'pffwc_image_padding_four'   => __( '4px', 'pffwc' ),
-					'pffwc_image_padding_five'   => __( '5px', 'pffwc' ),
-					'pffwc_image_padding_six'    => __( '6px', 'pffwc' ),
-					'pffwc_image_padding_seven'  => __( '7px', 'pffwc' ),
-					'pffwc_image_padding_eight'  => __( '8px', 'pffwc' ),
-				),
-			)
-		);
-		// Image background color
-		$wp_customize->add_setting( 'pffwc_image_bg',
-			array(
-				'default'           => '#fff',
-				'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
-		$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'pffwc_image_bg',
-			array(
-				'label'    => __( 'Background Color', 'pffwc' ),
-				'settings' => 'pffwc_image_bg',
-				'section'  => 'pffwc_options_fi',
-			)
-		) );
-
-		/*
-		 * Tooltip Options
-		 * @since 1.0
-		 */
-		// Tooltip Font Color
-		$wp_customize->add_setting( 'pffwc_tooltip_font_color',
-			array(
-				'default'           => '#fff',
-				'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
-		$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'pffwc_tooltip_font_color',
-			array(
-				'label'    => __( 'Font Color', 'pffwc' ),
-				'settings' => 'pffwc_tooltip_font_color',
-				'section'  => 'pffwc_options_tt',
-			)
-		) );
-		// Tooltip Background
-		$wp_customize->add_setting( 'pffwc_tooltip_bg',
-			array(
-				'default'           => '#333',
-				'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
-		$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'pffwc_tooltip_bg',
-			array(
-				'label'    => __( 'Background Color', 'pffwc' ),
-				'settings' => 'pffwc_tooltip_bg',
-				'section'  => 'pffwc_options_tt',
-			)
-		) );
-		// Tooltip Opacity
-		$wp_customize->add_setting( 'pffwc_tooltip_opacity',
-			array(
-				'default'           => '10',
-				'sanitize_callback' => '',
-			)
-		);
-		$wp_customize->add_control( 'pffwc_tooltip_opacity',
-			array(
-				'label'    => __( 'Background Opacity', 'pffwc' ),
-				'settings' => 'pffwc_tooltip_opacity',
-				'section'  => 'pffwc_options_tt',
-				'type'     => 'range',
-				'input_attrs' => array(
-					'min'     => 0,
-					'max'     => 10,
-					'steps'   => 1,
-				),
-			)
-		);
 	}
 
 	/**
-	 * pffwc Customizer callbacks/sanitize all the things
+	 * Customizer callbacks/sanitize all the things
 	 * @since 1.0
 	 */
 	// Location
@@ -279,68 +94,6 @@ class Pffwc_Settings {
 
 		if ( array_key_exists( $input, $pffwc_valid_location ) ) :
 			return $input;
-		else :
-			return '';
-		endif;
-	}
-
-	// Image Width
-	public function pffwc_sanitize_image_width( $input, $setting ) {
-		// $pffwc_valid_image_width = array(
-		// 	'pffwc_image_width_eight' => '8 Per Row',
-		// 	'pffwc_image_width_six'   => '6 Per Row',
-		// 	'pffwc_image_width_five'  => '5 Per Row',
-		// 	'pffwc_image_width_four'  => '4 Per Row',
-		// 	'pffwc_image_width_three' => '3 Per Row',
-		// 	'pffwc_image_width_two'   => '2 Per Row',
-		// );
-		//
-		// if ( array_key_exists( $input, $pffwc_valid_image_width ) ) :
-		// 	return $pffwc_valid_image_width;
-		// else :
-		// 	return '';
-		// endif;
-		// Ensure unput is a slub
-		$input = sanitize_key( $input );
-		$pffwc_choices = $setting->manager->get_control( $setting->id )->choices;
-		return ( array_key_exists( $input, $pffwc_choices ) ? $input : $setting->default );
-	}
-	// Border Width
-	public function pffwc_sanitize_border_width( $input ) {
-		$pffwc_valid_border_width = array(
-			'pffwc_image_border_zero'   => '0px',
-			'pffwc_image_border_one'    => '1px',
-			'pffwc_image_border_two'    => '2px',
-			'pffwc_image_border_three'  => '3px',
-			'pffwc_image_border_four'   => '4px',
-			'pffwc_image_border_five'   => '5px',
-			'pffwc_image_border_six'    => '6px',
-			'pffwc_image_border_seven'  => '7px',
-			'pffwc_image_border_eight'  => '8px',
-		);
-
-		if ( array_key_exists( $input, $pffwc_valid_border_width ) ) :
-			return $pffwc_valid_border_width;
-		else :
-			return '';
-		endif;
-	}
-	// Image padding
-	public function pffwc_sanitize_image_padding( $input ) {
-		$pffwc_valid_image_padding = array(
-			'pffwc_image_padding_zero'   => '0px',
-			'pffwc_image_padding_one'    => '1px',
-			'pffwc_image_padding_two'    => '2px',
-			'pffwc_image_padding_three'  => '3px',
-			'pffwc_image_padding_four'   => '4px',
-			'pffwc_image_padding_five'   => '5px',
-			'pffwc_image_padding_six'    => '6px',
-			'pffwc_image_padding_seven'  => '7px',
-			'pffwc_image_padding_eight'  => '8px',
-		);
-
-		if ( array_key_exists( $input, $pffwc_valid_image_padding ) ) :
-			return $pffwc_valid_image_padding;
 		else :
 			return '';
 		endif;
